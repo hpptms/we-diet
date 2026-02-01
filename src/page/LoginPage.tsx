c035b424fc5ba01319287d0b3f27bf495c4d73ce
冨田卓考 <takanori.tomita.pg@gmail.com>
2026-02-01 10:51:03 +0900

    Xログインボタンを表示するように変更

--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -431,7 +431,7 @@ const LoginPage: React.FC = () => {
                 gap: 1,
               }}>
                 <GoogleLoginButton />
-                {/* <XLoginButton /> */}
+                <XLoginButton />
                 <LineLoginButton />
                 {/* <FacebookLoginButton /> */}
                 {/* <TiktokLoginButton /> */}
